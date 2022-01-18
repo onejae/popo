aecb02dc58278ac5be8a99ba9455b1a155ae6108
wonjae lee <onejae@kbs.co.kr>
2022-01-19 04:02:38 +0900

    Init with springboot, kafka

---
+++ src/main/java/com/rightime/popo/PopoApplication.java
@@ -0,0 +1,13 @@
+package com.rightime.popo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rightime/popo/PopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rightime.popo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
