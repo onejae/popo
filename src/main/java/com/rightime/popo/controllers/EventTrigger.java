acc710a16743289c1c1fddff678d87ba6cec81cd
wonjae lee <onejae@kbs.co.kr>
2022-01-20 15:55:33 +0900

    Kafka version, mysql connector added, ticker class init

--- src/main/java/com/rightime/popo/controllers/EventTrigger.java
+++ src/main/java/com/rightime/popo/controllers/EventTrigger.java
@@ -1,7 +1,9 @@
 package com.rightime.popo.controllers;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EventTrigger {
     @Scheduled(cron = "*/5 * * * * *")
     public void tick() throws Exception {
